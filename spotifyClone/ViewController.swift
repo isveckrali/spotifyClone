4bb98fbe25df25d45051302415fc21fb359bb6d0
isveckrali <mehmet.can.seyhan1@gmail.com>
2024-11-13 18:52:49 +0100

    first commit

---
+++ spotifyClone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  spotifyClone
+//
+//  Created by Mehmet Seyhan on 13.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
